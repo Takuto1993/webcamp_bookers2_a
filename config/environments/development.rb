ccad48ffcec022c32589a67b8c6fcee6cedb7679
EC2 Default User <ec2-user@ip-172-31-40-91.ec2.internal>
2022-03-31 13:00:25 +0000

    [Add]ルーティングの記述変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c06242983a4b45fb8a7a9a685b36ebbe.vfs.cloud9.us-east-1.amazonaws.com"
 end
