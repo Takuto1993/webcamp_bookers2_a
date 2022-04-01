7dc31c9271b25e7fb01f9475bf58dc7d37322be5
EC2 Default User <ec2-user@ip-172-31-40-91.ec2.internal>
2022-04-01 13:06:33 +0000

    [Add]headerのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/top'
   root to: "homes#top"
   devise_for :users
-  resourses :books, only: [:new, :index,]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :book, only: [:new, :index, :show]
 end
