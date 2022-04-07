492f9667376ebcb6d71b0a9c9e2b316a97e6dd84
EC2 Default User <ec2-user@ip-172-31-40-91.ec2.internal>
2022-04-07 13:05:43 +0000

    [Add]userのeditページの作成（途中）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = @user.book
+    @book = @user.books
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: "about"
   devise_for :users
   resources :books, only: [:new, :create, :show]
-  resources :users, only: [:index, :edit, :update,]
+  resources :users, only: [:show, :index, :edit, :update,]
 end
